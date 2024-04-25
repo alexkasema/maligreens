77ac2ef3a6929de362aa213fcd212f2f50636ef5
AlexKasema <lexxmulwa@gmail.com>
2024-04-25 12:44:17 +0300

    Created endpoints to fetch data from backend using redux in the productsApiSlice

--- frontend/src/screens/product/ProductScreen.jsx
+++ frontend/src/screens/product/ProductScreen.jsx
@@ -1,25 +1,21 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button, Form} from 'react-bootstrap';
+import { useGetProductDetailsQuery } from '../../slices/productsApiSlice';
 import Rating from '../../components/Rating';
-
-import axios from 'axios';
+import Loader from '../../components/Loader';
+import Message from '../../components/Message';
 
 
 const ProductScreen = () => {
     const { id: productId } = useParams();
+
+    const { data:product, error, isLoading } = useGetProductDetailsQuery(productId);
     
-    const [product, setProduct] = useState({});
     const [qty, setQty] = useState(1);
 
-    useEffect(() => {
-        const fetchProduct = async() => {
-            const { data } = await axios.get(`/api/products/${productId}`);
-            setProduct(data);
-        }
-        fetchProduct();
-    }, [productId])
+
 
     const addToCartHandler = () => {
         console.log('Add to cart: ', product);
@@ -28,7 +24,13 @@ const ProductScreen = () => {
     return (
         <>
             <Link to='/' className='btn btn-light my-3'>Go Back</Link>
-            <Row>
+            {isLoading ? (
+            <Loader />) : error ? (
+                <Message variant='danger'>
+                    {error?.data?.message || error.error}
+                </Message>
+            ) : (
+                <Row>
                 <Col md={5}>
                     <Image src={product.imageUrls} alt={product.name} fluid/>
                 </Col>
@@ -103,6 +105,8 @@ const ProductScreen = () => {
                     </Card>
                 </Col>
             </Row>
+            )}
+            
         </>
         
     )
