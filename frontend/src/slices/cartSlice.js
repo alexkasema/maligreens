d5355f9dbbf45e56228a6e1ad390b4b1b5e2ac15
AlexKasema <lexxmulwa@gmail.com>
2024-04-26 11:57:03 +0300

    Created add to cart function in the cartSlice and exported it as an action that will be dispatched

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { updateCart } from "../utils/cartUtils";
 
 const initialState = localStorage.getItem('cart')
     ? JSON.parse(localStorage.getItem('cart'))
@@ -8,13 +9,28 @@ const cartSlice = createSlice({
     name: 'cart',
     initialState,
     reducers: {//! functions that have anything to do with the cart
+        addToCart: (state, action) => {
+            //! the state is whatever the current state is of the cart
+            //! any data inside of a payload (sending an item to add to the cart) which we can access
+            //! action.payload
+            const item = action.payload;
 
+            const existItem = state.cartItems.find(x => x._id === item._id);
+
+            if (existItem) {
+                state.cartItems = state.cartItems.map(x => x._id === existItem._id ? item : x);
+            } else {
+                state.cartItems = [...state.cartItems, item]
+            }
+
+            return updateCart(state);
+        },
     }
 });
 
 //? Then we need to export addToCart function as an ACTION
-export const { 
-    
+export const {
+    addToCart,
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
